e3395da7a699ce86d776daa59d8cb6e85fa5537a
Tyreworm <83973127+Tyreworm@users.noreply.github.com>
2021-10-10 11:06:10 +0100

    Add files via upload

--- StrixData2.py
+++ StrixData2.py
@@ -15,7 +15,7 @@ NAdata=[]
 EUWinData=[]
 Tolerance=0.1
 
-with open('Gladiator AM Season 3, Week 5 Melee.gg.csv', newline='') as csvfile:
+with open('The Great Big Gladiator Games.csv', newline='') as csvfile:
     alldecks = csv.reader(csvfile, delimiter=',', quotechar='|')
     for row in alldecks:
         deck = [row[0],row[1],row[2],row[3],row[4],row[5]]
@@ -56,62 +56,8 @@ with open('Gladiator AM Season 3, Week 5 Melee.gg.csv', newline='') as csvfile:
 
 WinData.pop(0)  
 
-with open('Gladiator EMEA Season 3, Week 5 Melee.gg.csv', newline='') as csvfile:
-    alldecks = csv.reader(csvfile, delimiter=',', quotechar='|')
-    for row in alldecks:
-        deck = [row[0],row[1],row[2],row[3],row[4],row[5]]
-        output=[]
-        for n in range(len(deck)):
-            deck[n]=deck[n].strip('"')
-               
-        
-        if deck[0]!='Rank':
-            
-            deckdata= [float(s) for s in re.findall(r'-?\d+\.?\d*', deck[4])]
-                  
-            deckwin= deckdata[0] 
-            deckloss =abs(deckdata[1] )
-            
-            deckperc=deck[5].strip('%')
-            deckperc=float(deckperc)
-            
-            PercCheck=(deckwin/(deckwin+deckloss))*100
-            
-            
-            
-            while PercCheck>(deckperc+Tolerance) or PercCheck<(deckperc-Tolerance):
-                deckwin=deckwin-1
-                deckloss = deckloss-1
-                PercCheck=(deckwin/(deckwin+deckloss))*100
-                
-            deck.append(deckwin)     
-            deck.append(deckloss)     
-            deck.append(deckperc) 
-            deck.append(PercCheck) 
-            output=[deck[1],deck[6],deck[7],'N']
-            
-        EUdata.append(deck) 
-        
-        for deckrow in WinData:
-            if deck[1]==deckrow[0]:
-                deckrow[1]=deckrow[1]+deckwin
-                deckrow[2]=deckrow[2]+deckloss
-                output[3]='Y'
-        
-      
-        EUWinData.append(output)        
-
-EUWinData.pop(0)  
-
-for row in EUWinData:
-    if row[3]=='N':
-        output=[row[0],row[1],row[2]]
-        WinData.append(output)
-        
-    
-        
-        
-with open('Week5.csv', 'w') as f:
+             
+with open('GBGG.csv', 'w') as f:
       
 
     write = csv.writer(f)
